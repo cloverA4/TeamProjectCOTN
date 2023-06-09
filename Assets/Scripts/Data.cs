79974e437a0ae380ed11895822f16c007cc306ae
kkh065 <kkh06500@gmail.com>
2023-06-09 10:29:50 +0900

    데이터작업, 플레이어cs 수정

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -12,12 +12,9 @@ public class Data : MonoBehaviour
     {
         if (null == instance)
         {
-            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
             instance = this;
 
             //씬 전환이 되더라도 파괴되지 않게 한다.
-            //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
-            //나는 헷갈림 방지를 위해 this를 붙여주기도 한다.
             DontDestroyOnLoad(this.gameObject);
         }
         else
@@ -65,14 +62,40 @@ public class Item
             _Name = value;
         }
     }
+}
+enum ItemType
+{
+    Shovel,
+    Weapon,
+    Armor,
+    Potion,
+}
 
-    enum ItemType
+enum CharacterState
+{
+    idle,
+    attack,
+    die,
+}
+public class PlayerCharacterData
+{
+    float _hp;
+
+    public float HP
     {
-        Shovel,
-        Weapon,
-        Armor,
-        Potion,
+        get { return _hp; }
+        set { _hp = value; }
     }
-    
 
+    float _maxHp;
+
+    public float MaxHP
+    {
+        get { return _maxHp; }
+        set { _maxHp = value; }
+    }
+
+    CharacterState state = CharacterState.idle;
+
+    List<Item> _equpedItemList = new List<Item>();
 }
