236c9be3a1a741a50d6a2c1d105a7de3ee34acb2
wisler99 <134022549+wisler99@users.noreply.github.com>
2023-07-28 02:18:58 +0900

    0728FailMessage

--- Assets/Scripts/UI/UIManeger.cs
+++ Assets/Scripts/UI/UIManeger.cs
@@ -40,12 +40,17 @@ public class UIManeger : MonoBehaviour
     [SerializeField] GameObject _retryToggle2;
 
     [SerializeField] GameObject _failMessage;
+    [SerializeField] GameObject _failMessageBase;
     [SerializeField] int _speed;
 
     private void Update()
     {
         if(_goLobbyUI.activeSelf == true) GoLobbyArrow();
         if (_alarmUI.activeSelf == true) AlarmArrow();
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(FailMessge());
+        }
         
     }
 
@@ -350,21 +355,23 @@ public class UIManeger : MonoBehaviour
 
     #endregion
 
-    public void FailMassge()
+    public IEnumerator FailMessge()
     { // 실패 메세지 뜨면 3초동안 올라가고
       // 점점 희미해지면서 사라짐
         float timer = 0;
-        GameObject temp = Instantiate(_failMessage);
-        float tempPos = temp.GetComponent<Transform>().position.y;
-        //while (true)
-        //{
-        //    timer += Time.deltaTime;
-        //    tempPos += _speed;
-        //    if(timer > 3)
-        //    {
-        //        Destroy(temp);
-        //        break;
-        //    }
-        //}
+        GameObject temp = Instantiate(_failMessage, _failMessageBase.transform);
+        Vector3 tempPos = temp.GetComponent<Transform>().position;
+        while(true)
+        {
+            timer += Time.deltaTime;
+            tempPos.y += _speed;
+            temp.GetComponent<Transform>().position = tempPos;
+            yield return null;
+            if(timer > 0.5)
+            {
+                break;
+            }
+        }
+        Destroy(temp);
     }
 }
