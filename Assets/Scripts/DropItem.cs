3c40ea08711e0dec1fb32a4586007dbc8fc54844
kkh065 <kkh06500@gmail.com>
2023-08-21 11:46:26 +0900

    상점아이템 판단값 추가

--- Assets/Scripts/DropItem.cs
+++ Assets/Scripts/DropItem.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class DropItem : MonoBehaviour
 {
     [SerializeField] SpriteRenderer _ItemIcon;
     Item _item;
+    bool _isShopItem = false;
     public Item Item 
     {
         private set { _item = value; }
         get { return _item; }
     }
 
-    public void Init(Item item)
+    public void Init(Item item, bool shop = false)
     {
         _item = item;
         _ItemIcon.sprite = _item._ItemIcon;
+        _isShopItem = shop;
     }
 }
