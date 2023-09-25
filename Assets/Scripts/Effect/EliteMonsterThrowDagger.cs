323acaee47d39c3879bc1aad2849043d29c90210
RMS98 <RMS>
2023-09-25 10:08:08 +0900

    엘리트 몬스터 특수공격 스크립트 수정중

--- Assets/Scripts/Effect/EliteMonsterThrowDagger.cs
+++ Assets/Scripts/Effect/EliteMonsterThrowDagger.cs
@@ -23,25 +23,28 @@ public class EliteMonsterThrowDagger : MonoBehaviour
 
     private void StartLaserAttack()
     {
-        Vector3 DaggerDirection = transform.forward;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, DaggerDirection, 100f, _obstacleLayer);
-        // 엘리트 몬스터 대거 발사루프
-        if (hit.collider != null)
+        if (gameObject != null)
         {
-            float laserLength = hit.distance;
-            Vector2 laserPosition = transform.position + (DaggerDirection * laserLength / 2f);
+            Vector3 DaggerDirection = transform.forward;
 
-            GameObject laserEffect = Instantiate(this.gameObject, laserPosition, Quaternion.identity);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, DaggerDirection, 100f, _obstacleLayer);
+            // 엘리트 몬스터 대거 발사루프
+            if (hit.collider != null)
+            {
+                float laserLength = hit.distance;
+                Vector2 laserPosition = transform.position + (DaggerDirection * laserLength / 2f);
 
-            // 레이저 이펙트의 길이 설정
-            laserEffect.transform.localScale = new Vector3(1f, laserLength, 1f);
+                GameObject laserEffect = Instantiate(this.gameObject, laserPosition, Quaternion.identity);
+
+                // 레이저 이펙트의 길이 설정
+                laserEffect.transform.localScale = new Vector3(1f, laserLength, 1f);
+
+                // 0.2초 후에 레이저 이펙트 삭제
+                Destroy(laserEffect, 0.2f);
+            }
 
             // 0.2초 후에 레이저 이펙트 삭제
-            Destroy(laserEffect, 0.2f);
+            Destroy(gameObject, 0.2f);
         }
-
-        // 레이저를 0.2초 후에 파괴
-        Destroy(gameObject, 0.2f);
     }
 }
