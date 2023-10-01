da42fd6feb7017f4ee926fdde01b0af503e6d63b
RMS98 <urin4444@gamil.com>
2023-10-02 05:14:16 +0900

    맵 세세한 레이어값오류 및 잘못설치된 블록 수정 미니맵 바닥 추가

--- Assets/Scripts/MiniMap/MinimapCamVer2.cs
+++ Assets/Scripts/MiniMap/MinimapCamVer2.cs
@@ -10,33 +10,38 @@ public class MinimapCamVer2 : MonoBehaviour
     [SerializeField] private Transform _stage1F3MinimapCamPos;
     [SerializeField] private Transform _stage1FBossMinimapCamPos;
 
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _stageLobbyFloor;
+    [SerializeField] private GameObject _stage1F1MinimapFloor;
+    [SerializeField] private GameObject _stage1F2MinimapFloor;
+    [SerializeField] private GameObject _stage1F3MinimapFloor;
+    [SerializeField] private GameObject _stage1FBossFloor;
 
-    
     void Update()
     {
         if (GameManager.Instance.NowStage == Stage.Lobby)
         {
             transform.position = _stageLobbyMinimapCamPos.position + new Vector3(0, 0, -10);
+            _stageLobbyFloor.SetActive(true);
         }
         else if ((GameManager.Instance.NowStage == Stage.Stage1) && (GameManager.Instance.NowFloor == floor.f1))
         {
             transform.position = _stage1F1MinimapCamPos.position + new Vector3(0, 0, -10);
+            _stage1F1MinimapFloor.SetActive(true);
         }
         else if ((GameManager.Instance.NowStage == Stage.Stage1) && (GameManager.Instance.NowFloor == floor.f2))
         {
             transform.position = _stage1F2MinimapCamPos.position + new Vector3(0, 0, -10);
+            _stage1F2MinimapFloor.SetActive(true);
         }
         else if ((GameManager.Instance.NowStage == Stage.Stage1) && (GameManager.Instance.NowFloor == floor.f3))
         {
             transform.position = _stage1F3MinimapCamPos.position + new Vector3(0, 0, -10);
+            _stage1F3MinimapFloor.SetActive(true);
         }
         else if ((GameManager.Instance.NowStage == Stage.Stage1) && (GameManager.Instance.NowFloor == floor.fBoss))
         {
             transform.position = _stage1FBossMinimapCamPos.position + new Vector3(0, 0, -10);
+            _stage1FBossFloor.SetActive(true);
         }
     }
 }
